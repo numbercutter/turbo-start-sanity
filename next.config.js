f21d99bf932040e93b6151a478104e65f6d72584
Raymond <raymond.luke.spartz@gmail.com>
2025-03-17 08:09:47 -0500

    damn

--- next.config.js
+++ next.config.js
@@ -1,1 +1,20 @@
- 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Update experimental options to remove reactRoot
+  experimental: {
+    // reactRoot is no longer needed in Next.js 15
+    serverActions: {
+      bodySizeLimit: "2mb",
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
